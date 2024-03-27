7b14ebab0cb93417904ee53a62034c6e9b1df7f9
srishti-digi <smishra@digisprint.com>
2024-03-27 14:59:14 +0530

    module created

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,10 @@
 var http = require("http");
+var dt = require("./myfirstmodule");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
   })
   .listen(8080);
