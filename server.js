cb93bba93d6b3893acfe4f8c1709dddc80dced2e
srishti-digi <smishra@digisprint.com>
2024-03-28 12:35:49 +0530

    added get method

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   console.log("Hello");
-  res.render("index");
+  res.render("index", { text: "all" });
+});
+
+app.get("/users", (req, res) => {
+  res.send("users list");
+});
+
+app.get("/users/new", (req, res) => {
+  res.send("users new list");
 });
 app.listen(3000);
